c5e6241d99ee2b4dcca9aeab45bfdd9bad22953d
BuildTools <unconfigured@null.spigotmc.org>
2017-02-22 00:14:53 +0900

    man10anni with config read function(simple)

--- src/main/java/red/man10/man10anni.java
+++ src/main/java/red/man10/man10anni.java
@@ -6,10 +6,18 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
-import ru.tehkode.permissions.PermissionUser;
-import ru.tehkode.permissions.bukkit.PermissionsEx;
 
 public final class man10anni extends JavaPlugin implements Listener {
+
+    String server_ip = this.getConfig().getString("server_ip");
+    String server_port = this.getConfig().getString("server_port");
+    String ssh_port = this.getConfig().getString("ssh_port");
+    String ssh_id = this.getConfig().getString("ssh_id");
+    String ssh_pass = this.getConfig().getString("ssh_pass");
+    String mysql_id = this.getConfig().getString("mysql_id");
+    String mysql_pass = this.getConfig().getString("mysql_pass");
+    String item_xp = this.getConfig().getString("item_xp");
+
 
     @Override
     public void onEnable() {
@@ -17,7 +25,6 @@ public final class man10anni extends JavaPlugin implements Listener {
         this.getServer().getPluginManager().registerEvents(this, this);
         getLogger().info("annixp has been enabled");
         this.saveDefaultConfig();
-
     }
 
     @Override
@@ -30,21 +37,38 @@ public final class man10anni extends JavaPlugin implements Listener {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         Player p = (Player) sender;
         if (cmd.getName().equalsIgnoreCase("annitop")) {
-            PermissionUser user = PermissionsEx.getUser(p);
-            if(user.has("man10.annixp.test")){
-                //権限があったらここに来る
-                p.sendMessage("you have permission");
+            //action annitop command
+
+        }else if(cmd.getName().equalsIgnoreCase("manni")){
+            if(args.length >= 1){
+                if(args[0].equalsIgnoreCase("help")){
+                    //action manni help command
+
+
+                }else if(args[0].equalsIgnoreCase("reload")){
+                    configReload();
+                    p.sendMessage("reloaded config");
+
+                }
+
             }else{
-                //無かったらこっち
-                p.sendMessage("you don't have permission");
+                p.sendMessage("wrong command usage do /manni help for a list of commands");
             }
-        }else if(cmd.getName().equalsIgnoreCase("configreload")){
-            this.reloadConfig();
-            p.sendMessage("config reloaded");
         }
         return true;
     }
 
+    public void configReload(){
+        this.reloadConfig();
+        server_ip = this.getConfig().getString("server_ip");
+        server_port = this.getConfig().getString("server_port");
+        ssh_port = this.getConfig().getString("ssh_port");
+        ssh_id = this.getConfig().getString("ssh_id");
+        ssh_pass = this.getConfig().getString("ssh_pass");
+        mysql_id = this.getConfig().getString("mysql_id");
+        mysql_pass = this.getConfig().getString("mysql_pass");
+        item_xp = this.getConfig().getString("item_xp");
+    }
 
 
 }
