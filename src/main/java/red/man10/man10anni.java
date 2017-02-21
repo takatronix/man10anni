73120dffa5cbe3d02eee67bcc98f36498c74c073
BuildTools <unconfigured@null.spigotmc.org>
2017-02-21 20:56:16 +0900

    permission ex lib example build by sho

--- src/main/java/red/man10/man10anni.java
+++ src/main/java/red/man10/man10anni.java
@@ -1,10 +1,16 @@
 package red.man10;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import ru.tehkode.permissions.PermissionUser;
 import ru.tehkode.permissions.bukkit.PermissionsEx;
+
+import java.util.UUID;
+
 
 public final class man10anni extends JavaPlugin implements Listener {
 
@@ -12,24 +18,36 @@ public final class man10anni extends JavaPlugin implements Listener {
     public void onEnable() {
         // Plugin startup logic
         this.getServer().getPluginManager().registerEvents(this, this);
+        getLogger().info("annixp has been enabled");
+        this.saveDefaultConfig();
 
     }
 
     @Override
     public void onDisable() {
+        getLogger().info("annixp hjas been disabled");
         // Plugin shutdown logic
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        Player p = (Player) sender;
         if (cmd.getName().equalsIgnoreCase("annitop")) {
-            //annitop 処理 by sho0
-            //takatornix追加 shoありがと
-            PermissionsEx e = new PermissionsEx();
-            //  ビルドできたぽい -> discord通知テスト２
-            //　discord 通知テスト by sho
-            //　discord 通知テスト2 by sho
+            PermissionUser user = PermissionsEx.getUser(p);
+            if(user.has("man10.annixp.test")){
+                //権限があったらここに来る
+                p.sendMessage("you have permission");
+            }else{
+                //無かったら
+                p.sendMessage("you don't have permission");
+            }
+        }else if(cmd.getName().equalsIgnoreCase("configreload")){
+            this.reloadConfig();
+            p.sendMessage("config reloaded");
         }
         return true;
     }
+
+
+
 }
